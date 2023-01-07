d3b5a963ef87037a10f79813a3ae519b692901b2
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-07 17:52:38 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="App">
+    <footer>
+      &copy; Awesome Company
+    </footer>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// import Mainpage from "@/view_user/pages_user/Mainpage";
+export default {
+  name: 'App',
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  components: {
+    // Mainpage
+
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+};
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="inspire" >
+    <Header v-model="expandOnHover"></Header>
+    <Sidebar :expand-on-hover.sync="expandOnHover"></Sidebar>
+    <v-content style="background-color: white">
+      <headerIntro></headerIntro>
+      <v-container rounded-0 class="fill-height" fluid>
+        <router-view />
+
+      </v-container>
+    </v-content>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+// import Header from "./header/Header";
+// import Sidebar from "./sidebar/Sidebar";
+import { mapState, mapMutations } from "vuex";
+import userHeader from "./Header/userHeader";
+import Footer from "./footer/Footer";
+
+export default {
+  name: "Layout",
+
+  components: {
+    // Header,
+    userHeader,
+    Footer
+  }
+};
+</script>
+
+<style>
+.fill-height {
+  align-items: start !important;
+  margin-top: 42px;
+}
+</style>
