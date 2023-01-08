ebcf8ed8a150b4c85428ecde7419bea38ad70963
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-08 23:50:38 +0700

    slider

--- src/components/products/Slider.vue
+++ src/components/products/Slider.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="slider">
     <div class="grid wide">
-      <img src="@/assets/images/desk-header.png" style="width: 100%" alt="">
+      <img src="https://images.fpt.shop/unsafe/fit-in/filters:quality(80):fill(transparent)/fptshop.com.vn/Uploads/Originals/2022/12/26/638076621909941793_desk-header.png"
+           style="width: 100%" alt="">
     </div>
     <div class="grid wide slider">
       <div class="row">
-        <div class="col l-8 m-12 c-12">
+        <div class="col l-12 m-12 c-12">
           <!-- <div class="slider"> -->
           <div class="grid">
             <div class="wapper-slider">
               <div class="row no-warp main-slider">
                 <div class="col l-12 m-12 c-12 wrapper-item-slider">
                   <div class="item-slider">
-                    <img src="@/assets/img/637806895646282188_F-H1_800x300.jfif" alt="">
+                    <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/14/638066548736519019_F-H6_1190x300.png"
+                         alt="">
                   </div>
                 </div>
                 <div class="col l-12 m-12 c-12 wrapper-item-slider">
                   <div class="item-slider">
-                    <img src="@/assets/img/slide-3.png" alt="">
+                    <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/19/638070597111862277_H6%20-%201190x300.png"
+                         alt="">
                   </div>
                 </div>
                 <div class="col l-12 m-12 c-12 wrapper-item-slider">
                   <div class="item-slider">
-                    <img src="@/assets/img/637811231329170645_F-H1_800x300.jfif" alt="">
+                    <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/15/638067214883821844_F-H6_1190x300.png"
+                         alt="">
+                  </div>
+                </div>
+                <div class="col l-12 m-12 c-12 wrapper-item-slider">
+                  <div class="item-slider">
+                    <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/22/638073138777718013_H6%20-%201190x300.png"
+                         alt="">
                   </div>
                 </div>
               </div>
@@ -39,16 +49,6 @@
             <span class="dot"></span>
             <span class="dot"></span>
             <span class="dot"></span>
-          </div>
-        </div>
-        <div class="col l-4 c-12 m-12">
-          <div class="banner-top">
-            <div class="banner-img-item">
-              <img src="@/assets/img/slide-1.png" alt="">
-            </div>
-            <div class="banner-img-item">
-              <img src="@/assets/img/slide-2.png" alt="">
-            </div>
           </div>
         </div>
       </div>
