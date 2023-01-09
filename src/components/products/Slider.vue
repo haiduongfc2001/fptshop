242ea0f07e9e5167ade8bd15dc624d4776d1d8cc
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-10 01:32:11 +0700

    add

--- src/components/products/Slider.vue
+++ src/components/products/Slider.vue
@@ -6,61 +6,53 @@
              style="width: 100%" alt="">
       </div>
       <div class="grid wide slider">
-        <b-carousel
-            id="carousel-1"
-            v-model="slide"
-            :interval="4000"
-            controls
-            indicators
-            background="#ababab"
-            img-width="1024"
-            img-height="480"
-            style="text-shadow: 1px 1px 2px #333;"
-            @sliding-start="onSlideStart"
-            @sliding-end="onSlideEnd"
-        >
-          <!-- Text slides with image -->
-          <b-carousel-slide
-              caption="First slide"
-              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-              img-src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/14/638066548736519019_F-H6_1190x300.png"
-          ></b-carousel-slide>
+        <div class="row">
+          <div class="col l-12 m-12 c-12">
+            <!-- <div class="slider"> -->
+            <div class="grid">
+              <div class="wapper-slider">
+                <div class="row no-warp main-slider">
+                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
+                    <div class="item-slider">
+                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/14/638066548736519019_F-H6_1190x300.png"
+                           alt="">
+                    </div>
+                  </div>
+                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
+                    <div class="item-slider">
+                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/19/638070597111862277_H6%20-%201190x300.png"
+                           alt="">
+                    </div>
+                  </div>
+                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
+                    <div class="item-slider">
+                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/15/638067214883821844_F-H6_1190x300.png"
+                           alt="">
+                    </div>
+                  </div>
+                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
+                    <div class="item-slider">
+                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/22/638073138777718013_H6%20-%201190x300.png"
+                           alt="">
+                    </div>
+                  </div>
+                </div>
+                <button class="back">
+                  <i class="fa fa-chevron-left"></i>
+                </button>
+                <button class="next">
+                  <i class="fa fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>
 
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Hello world!</h1>
-          </b-carousel-slide>
-
-          <!-- Slides with image only -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-          <!-- Slides with img slot -->
-          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-          <b-carousel-slide>
-            <template #img>
-              <img
-                  class="d-block img-fluid w-100"
-                  width="1024"
-                  height="480"
-                  src="https://picsum.photos/1024/480/?image=55"
-                  alt="image slot"
-              >
-            </template>
-          </b-carousel-slide>
-
-          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-          <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-              a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-            </p>
-          </b-carousel-slide>
-        </b-carousel>
-
-        <p class="mt-4">
-          Slide #: {{ slide }}<br>
-          Sliding: {{ sliding }}
-        </p>
+            <div style="text-align:center">
+              <span class="dot"></span>
+              <span class="dot"></span>
+              <span class="dot"></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -68,21 +60,7 @@
 
 <script>
 export default {
-  name: "Slider",
-  data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
-  },
-  methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      }
-  }
+  name: "Slider"
 }
 </script>
 
