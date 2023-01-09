fc598c9f1fad3a105319d159a989ea3a3be7d681
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-09 23:03:16 +0700

    slider

--- src/components/products/Slider.vue
+++ src/components/products/Slider.vue
@@ -6,45 +6,25 @@
              style="width: 100%" alt="">
       </div>
       <div class="grid wide slider">
-        <div class="row">
-          <div class="col l-12 m-12 c-12">
-            <!-- <div class="slider"> -->
-            <div class="grid">
-              <div class="wapper-slider">
-                <div class="row no-warp main-slider">
-                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
-                    <div class="item-slider">
-                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/14/638066548736519019_F-H6_1190x300.png"
-                           alt="">
-                    </div>
-                  </div>
-                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
-                    <div class="item-slider">
-                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/19/638070597111862277_H6%20-%201190x300.png"
-                           alt="">
-                    </div>
-                  </div>
-                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
-                    <div class="item-slider">
-                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/15/638067214883821844_F-H6_1190x300.png"
-                           alt="">
-                    </div>
-                  </div>
-                  <div class="col l-12 m-12 c-12 wrapper-item-slider">
-                    <div class="item-slider">
-                      <img src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/22/638073138777718013_H6%20-%201190x300.png"
-                           alt="">
-                    </div>
-                  </div>
-                </div>
-                <button class="back">
-                  <i class="fa fa-chevron-left"></i>
-                </button>
-                <button class="next">
-                  <i class="fa fa-chevron-right"></i>
-                </button>
-              </div>
-            </div>
+        <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333;"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+        >
+          <!-- Text slides with image -->
+          <b-carousel-slide
+              caption="First slide"
+              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+              img-src="https://images.fpt.shop/unsafe/fit-in/1190x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/12/14/638066548736519019_F-H6_1190x300.png"
+          ></b-carousel-slide>
 
           <!-- Slides with custom text -->
           <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
